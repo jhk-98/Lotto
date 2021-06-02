5d1de451c532681f323301bb7a2c47bee900d6a2
JunHyuk <kkjun221@naver.com>
2021-06-02 11:43:46 +0900

    Hash Code based on DatePicker

--- app/src/main/java/com/example/lotto/ConstellationActivity.kt
+++ app/src/main/java/com/example/lotto/ConstellationActivity.kt
@@ -4,33 +4,62 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.CalendarView
 import android.widget.DatePicker
 import android.widget.TextView
-import org.w3c.dom.Text
 import java.text.SimpleDateFormat
 import java.util.*
+import kotlin.collections.ArrayList
+
+fun getLottoNumbersFromHash(str: String): MutableList<Int>{
+
+    val list = mutableListOf<Int>()
+    for (number in 1..45){
+        list.add(number)
+    }
+    val targetString = SimpleDateFormat("yyyy-MM-dd-HH-mm-ss-SS", Locale.KOREA).format(Date()) + str
+
+    list.shuffle(Random(targetString.hashCode().toLong())) // 같은 seed 사용하면 항상 같은 sequence
+
+    return list.subList(0, 6)
+}
+
 
 class ConstellationActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_constellation)
 
-        val btnGoConstell = findViewById<Button>(R.id.btnGoResultConstell)
-        var intent : Intent
-        val datePicker = findViewById<DatePicker>(R.id.date_picker_actions)
+        val btnGoResult = findViewById<Button>(R.id.btnGoResultConstell)
+        val datePicker = findViewById<DatePicker>(R.id.datePicker)
         val txtConstell = findViewById<TextView>(R.id.txtConstell)
-        txtConstell.text = makeConstellationString(datePicker.month, datePicker.dayOfMonth)
+        txtConstell.text = makeConstellString(datePicker.month, datePicker.dayOfMonth)
 
-        btnGoConstell.setOnClickListener {
-            intent = Intent(this@ConstellationActivity, ResultActivity::class.java)
-            intent.putIntegerArrayListExtra("result",ArrayList(getLottoNumbersFromHash(makeConstellationString(datePicker.month, datePicker.dayOfMonth))))
-            intent.putExtra("constellation",makeConstellationString(datePicker.month, datePicker.dayOfMonth))
-            startActivity(intent)
+        btnGoResult.setOnClickListener {
+            val intent = Intent(this, ResultActivity::class.java)
+            intent.putIntegerArrayListExtra("result", ArrayList(getLottoNumbersFromHash(txtConstell.text.toString())))
+            startActivity(Intent(this, ResultActivity::class.java))
         }
+        val calendar = Calendar.getInstance()
+
+        datePicker.init(calendar.get(Calendar.YEAR),calendar.get(Calendar.MONTH),calendar.get(Calendar.DAY_OF_MONTH),
+                object : CalendarView.OnDateChangeListener, DatePicker.OnDateChangedListener{
+                    override fun onSelectedDayChange(view: CalendarView, year: Int, month: Int, dayOfMonth: Int) {
+                        TODO("Not yet implemented")
+                    }
+
+                    override fun onDateChanged(view: DatePicker?, year: Int, monthOfYear: Int, dayOfMonth: Int) {
+                        txtConstell.text = makeConstellString(datePicker.month,datePicker.dayOfMonth)
+                    }
+                }
+        )
+        }
+
     }
 
-    private fun makeConstellationString(month: Int, day: Int): String {
-        val target = "${month+1}${String.format("%02d",day)}".toInt()
+    private fun makeConstellString(month: Int, dayOfMonth: Int): String {
+
+        val target = "${month + 1}${String.format("%02d",dayOfMonth)}".toInt()
 
         when (target){
             in 101..119 -> return "염소자리"
@@ -50,19 +79,3 @@ class ConstellationActivity : AppCompatActivity() {
         }
     }
 
-    fun getLottoNumbersFromHash(str: String): MutableList<Int>{
-        // 정수 list todtjd
-        val list = mutableListOf<Int>()
-        // list에 정수 저장
-        for (number in 1..45){
-            list.add(number)
-        }
-
-        val targetString = SimpleDateFormat("yyyy-MM-dd-HH-mm-ss-SS", Locale.KOREA).format(Date()) + str
-
-        // list 섞기. SEED 값으로 이름의 hash 코드 사용
-        list.shuffle(Random(targetString.hashCode().toLong())) // 같은 seed 사용하면 항상 같은 sequence
-        // 앞에서부터 6개 반환
-        return list.subList(0, 6)
-    }
-}
